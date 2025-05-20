7116e87ccd3b66d0ad3513ab2ffed117de146339
ItRealRew <rew-proninmax@yandex.ru>
2025-05-20 20:50:41 +0300

    Finished

--- react-gh/src/App.js
+++ react-gh/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={logo} className="logo" alt="logo" />
+        <h1>
+         Custom33
+        </h1>
+        <div className='link_wrap'>
+          <a href='https://t.me/bpancustom33' target="_blank">Telegram</a>
+          <a href='https://www.instagram.com/bpancustom33' target="_blank">Instagram</a>
+        </div>
       </header>
     </div>
   );
